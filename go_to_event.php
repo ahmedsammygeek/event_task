e0075cf44b011cdf02eb7472958dc4f9e96925dd
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-04-27 16:01:01 +0200

    now the user can register to event

--- go_to_event.php
+++ go_to_event.php
@@ -1,10 +1,10 @@
 <?php  session_start();
 
-if(!isset($_GET['id'])) {
+if(!isset($_GET['event_id'])) {
 	die("error happend pelse try again");
 } 
 
-$event_id = $_GET['id'];
+$event_id = $_GET['event_id'];
 
 $connect = mysqli_connect('localhost', "root", "", "event_task");
 $query = mysqli_query($connect, "SELECT * FROM events WHERE id='$event_id'");
@@ -33,15 +33,23 @@ if(!isset($_POST['email']) || empty($_POST['email'])) {
 if(!isset($_POST['phone']) || empty($_POST['phone'])) {
 	die("please enter your phone");
 }
-
 $username = $_POST['username'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
-
-
-
 $user_id = $_SESSION['user_id'];
-$query = mysqli_query($connect, "INSERT INTO even_users VALUES('' , '$event_id' , '' )");
+$query = mysqli_query($connect, "INSERT INTO even_users VALUES('' , '$event_id' , '$username' , '$email'
+ , '$phone' , '$user_id')");
+
+
+if($query) {
+	echo "done , you now had registered to the event";
+	die();
+}
+
+else {
+	echo "please try again";
+	die();
+}
 
 
 
